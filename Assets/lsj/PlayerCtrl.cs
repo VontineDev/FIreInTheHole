585d7a15cb945ac276a90db0e3a229e6af0d6d1d
lee <powerlsj7@gmail.com>
2021-05-12 18:34:38 +0900

    lsj
    일단 커밋

---
+++ Assets/lsj/PlayerCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public GameObject ball;
+    public Transform tr;
+    public Dimension d1;
+    public Dimension d2;
+    // Start is called before the first frame update
+    private void Update()
+    {
+      //  tr.position = Vector3.zero;
+        if(OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            CreateBall();
+        }
+    }
+    
+
+    public void CreateBall()
+    {
+        Instantiate(ball, transform.position, transform.rotation);
+        //ball.GetComponent<>
+    }
+}
---
+++ Assets/lsj/PortalCtrl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PortalCtrl:MonoBehaviour
+{
+    /// <summary>
+    /// 볼
+    /// </summary>
+    public GameObject ball;
+    public Rigidbody ballRig;
+    public MeshRenderer render;
+
+    /// <summary>
+    /// 포탈
+    /// </summary>
+    public GameObject portal;
+    /// <summary>
+    /// 생성될 거리크기
+    /// </summary>
+    public float distance;
+    /// <summary>
+    /// 플레이어 위치
+    /// </summary>
+    //public Transform player;
+    public bool isCreate=false;
+    // Start is called before the first frame update
+   public void Updates()
+    {
+        if(isCreate)
+        {
+            return;
+        }
+        if (distance <= GetDistance())
+        {
+            Debug.Log("포탈생성");
+            OffRenderer();
+            isCreate = true;
+        }
+    }
+
+    public float GetDistance()
+    {
+        return Vector3.Distance(Vector3.zero, ball.transform.position);
+    }
+    public void OffRenderer()
+    {
+        render.enabled = false;
+        //   SetActiveObj(true, portal);
+        portal.SetActive(true);
+        ballRig.velocity = Vector3.zero;
+    }
+    
+}

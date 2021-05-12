cc3639e94d12ff7b282a983e6d04844031dcf111
VontineDev <57723128+VontineDev@users.noreply.github.com>
2021-05-12 17:57:27 +0900

    New OVR Controller Prefab Added
    it has Hand Model

---
+++ Assets/Scripts/CreateWaterBalloon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWaterBalloon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("CreateWaterBalloon");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        {
+            print($"PrimaryHandTrigger Down");
+
+        }
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            print($"SecondaryHandTrigger Down");
+
+        }
+    }
+}

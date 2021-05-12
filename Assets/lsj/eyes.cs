4208e1c65fc7712f208eea4ffe7927e1c3fc7d22
powerlsj7 <46619303+powerlsj7@users.noreply.github.com>
2021-05-13 00:47:26 +0900

    rotation
    에셋 frement? 에셋 2개 추가됨
    유리 쪼개지는 에셋, 콜리더 조각 에셋
    lsj폴더에 스크립트 추가.

---
+++ Assets/lsj/eyes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eyes : MonoBehaviour
+{
+    public Transform eyePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveRotate();
+    }
+    void MoveRotate()
+    {
+        //  Quaternion q = Quaternion.Euler(360f - eyePos.rotation.x, 360f - eyePos.rotation.y, 360f - eyePos.rotation.z);
+     //   transform.eulerAngles =new Vector3(360f - Mathf.Abs(eyePos.eulerAngles.x), 360f - Mathf.Abs(eyePos.eulerAngles.y), 360f - Mathf.Abs(eyePos.eulerAngles.z));
+    }
+}

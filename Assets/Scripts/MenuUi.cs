667bddf7b34a67701ce672d5200f30b32cfd98d6
babodya <jeh016@naver.com>
2021-05-14 22:02:13 +0900

    eunhee-3

---
+++ Assets/Scripts/MenuUi.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUi : MonoBehaviour
+{
+    public Camera viewCamera;
+
+    void Update()
+    {
+        if (viewCamera != null)
+        {
+            this.transform.position = viewCamera.transform.position;
+            this.transform.rotation = viewCamera.transform.rotation;
+
+        }
+    }
+}

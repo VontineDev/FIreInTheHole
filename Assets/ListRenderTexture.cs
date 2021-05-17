17fef312a09755a3bb086b08c39c55c2977de60c
VontineDev <57723128+VontineDev@users.noreply.github.com>
2021-05-17 10:52:58 +0900

    Revert "Revert "Hmm...""
    This reverts commit 4a76ab113eede022fec544522ea1eac5d14b2e86.

---
+++ Assets/ListRenderTexture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListRenderTexture : MonoBehaviour
+{
+    public static ListRenderTexture Instance
+    {
+        get; set;
+    }
+    public List<RenderTexture> listRT;
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        listRT = new List<RenderTexture>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/SphereController.cs
+++ Assets/SphereController.cs
@@ -1,76 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class SphereController : MonoBehaviour
 {
     [SerializeField]
     OVRCameraRig OVRCameraRig;
-
     [SerializeField]
-    GameObject quadPrefab;
-    [SerializeField]
-    GameObject cameraPrefab;
-    [SerializeField]
-    Transform originalCameraTransform;
+    Transform origiCamTr;
     [SerializeField]
     Transform World2;
-    [SerializeField]
-    RenderTexture copyRenderTexture;
-    [SerializeField]
-    Material materialPrefab;
     // Start is called before the first frame update
     void Start()
     {
         OVRCameraRig = FindObjectOfType<OVRCameraRig>();
         World2 = GameObject.FindGameObjectWithTag("World2").transform;
 
+        origiCamTr = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
         StartCoroutine(DestroySelf());
     }
     IEnumerator DestroySelf()
     {
         float time = 0;
-        while (time < 5f)
+        while (time < 7f)
         {
             time += Time.deltaTime;
             yield return null;
             if (Vector3.Distance(this.transform.position, OVRCameraRig.transform.position) > 5)
             {
-                //var portal = Instantiate(portalPrefab);
-                //var ps = portal.GetComponentInChildren<Portal>();
-                //var dim = FindObjectsOfType<Dimension>();
-                //ps.dimension1 = dim[1];
-                //ps.dimension2 = dim[0];
-                //portal.transform.position = this.transform.position;
-                //portal.transform.LookAt(OVRCameraRig.transform);
-                var quad = Instantiate(quadPrefab);
-                quad.transform.position = this.transform.position;
-                quad.transform.LookAt(OVRCameraRig.transform);
-                quad.transform.Rotate(0, 180f, 0);
-                RenderTexture renderTexture = new RenderTexture(1024, 1024, 16, RenderTextureFormat.ARGB32);
 
+                RenderTexture renderTexture = new RenderTexture(512, 512, 16, RenderTextureFormat.ARGB32);
                 renderTexture.Create();
-                var meshrend = quad.GetComponent<MeshRenderer>();
-                meshrend.material = new Material(materialPrefab);
-                meshrend.material.mainTexture = renderTexture;
-                var camObject = Instantiate(cameraPrefab, World2.transform.position, World2.transform.rotation);
-                var camera = camObject.GetComponent<Camera>();
+                ListRenderTexture.Instance.listRT.Add(renderTexture);
+                if (renderTexture.IsCreated())
+                {
+                    //Make Quad
+                    var quadGo = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    quadGo.transform.position = this.transform.position;
+                    var quad = quadGo.AddComponent<QuadSetter>();
+                  
+                    var savePos = quad.transform.position;
+                    var cameraGo = new GameObject("Camera");
+                    var camera = cameraGo.AddComponent<Camera>();
+                    camera.transform.position = World2.transform.position;
+                    var pos = camera.transform.position + savePos;
+                    print($"{pos}  Cameara Look AT");
+                    camera.transform.LookAt(pos);
+                    camera.targetTexture = renderTexture;
+                    camera.fieldOfView = 12.2f;
 
-                camera.targetTexture = renderTexture;
-                camera.fieldOfView = 12f;
+                    quad.Init(this.transform.position, origiCamTr, renderTexture);
+
+                }
                 break;
             }
         }
-        // yield return new WaitForSeconds(5f);
         Destroy(this.gameObject);
     }
-    private void OnTriggerExit(Collider other)
+    private Texture2D RenderTextureTo2DTexture(RenderTexture rt)
     {
-        if (other.tag == "World")
-        {
-            print("Sphere Triggered with World");
-            print(this.transform.position);
-        }
+        var texture = new Texture2D(rt.width, rt.height, rt.graphicsFormat, 0, TextureCreationFlags.None);
+        RenderTexture.active = rt;
+        texture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        texture.Apply();
+
+        //RenderTexture.active = null;
+
+        return texture;
     }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "World")
+    //    {
+    //        print("Sphere Triggered with World");
+    //        print(this.transform.position);
+    //    }
+    //}
 
 }
